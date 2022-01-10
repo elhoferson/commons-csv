f61a517672b877eeb13968befd41317db830daa4
Alexander Hofer <alehofer@edu.aau.at>
2022-01-10 11:37:14 +0100

    test 3

---
+++ src/test/java/org/apache/commons/csv/issues/JiraCsv198Test.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.csv.issues;
+
+import org.apache.commons.csv.format.CSVFormat;
+import org.apache.commons.csv.parser.CSVParser;
+import org.apache.commons.csv.parser.ICSVParser;
+import org.apache.commons.csv.record.CSVRecord;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class JiraCsv198Test {
+
+    // @formatter:off
+    private static final CSVFormat CSV_FORMAT = CSVFormat.EXCEL.builder()
+        .setDelimiter('^')
+        .setHeader()
+        .setSkipHeaderRecord(true)
+        .build();
+    // @formatter:on
+
+    @Test
+    @Disabled
+    public void test() throws UnsupportedEncodingException, IOException {
+        final InputStream pointsOfReference = getClass()
+            .getResourceAsStream("/org/apache/commons/csv/CSV-198/optd_por_public.csv");
+        assertNotNull(pointsOfReference);
+        try (@SuppressWarnings("resource")
+             ICSVParser parser = new CSVParser(new InputStreamReader(pointsOfReference, StandardCharsets.UTF_8), CSVFormat.DEFAULT)) {
+            for (final CSVRecord record : parser) {
+                final String locationType = record.get("location_type");
+                assertNotNull(locationType);
+            }
+        }
+    }
+
+}
---
+++ src/test/java/org/apache/commons/csv/issues/JiraCsv206Test.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.csv.issues;
+
+import org.apache.commons.csv.format.CSVFormat;
+import org.apache.commons.csv.parser.CSVParser;
+import org.apache.commons.csv.parser.ICSVParser;
+import org.apache.commons.csv.printer.CSVPrinter;
+import org.apache.commons.csv.record.CSVRecord;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JiraCsv206Test {
+
+    @Test
+    public void testJiraCsv206MultipleCharacterDelimiter() throws IOException {
+        // Read with multiple character delimiter
+        final String source = "FirstName[|]LastName[|]Address\r\nJohn[|]Smith[|]123 Main St.";
+        final StringReader reader = new StringReader(source);
+        final CSVFormat csvFormat = CSVFormat.DEFAULT.builder().setDelimiter("[|]").build();
+        CSVRecord record = null;
+        try (final ICSVParser ICSVParser = new CSVParser(reader, csvFormat)) {
+            final Iterator<CSVRecord> iterator = ICSVParser.iterator();
+            record = iterator.next();
+            assertEquals("FirstName", record.get(0));
+            assertEquals("LastName", record.get(1));
+            assertEquals("Address", record.get(2));
+            record = iterator.next();
+            assertEquals("John", record.get(0));
+            assertEquals("Smith", record.get(1));
+            assertEquals("123 Main St.", record.get(2));
+        }
+        // Write with multiple character delimiter
+        final String outString = "# Change delimiter to [I]\r\n" + "first name[I]last name[I]address\r\n"
+            + "John[I]Smith[I]123 Main St.";
+        final String comment = "Change delimiter to [I]";
+        // @formatter:off
+        final CSVFormat format = CSVFormat.EXCEL.builder()
+                .setDelimiter("[I]").setHeader("first name", "last name", "address")
+                .setCommentMarker('#')
+                .setHeaderComments(comment).build();
+        // @formatter:on
+        final StringBuilder out = new StringBuilder();
+        try (final CSVPrinter printer = new CSVPrinter(out, format)) {
+            printer.print(record.get(0));
+            printer.print(record.get(1));
+            printer.print(record.get(2));
+        }
+        final String s = out.toString();
+        assertEquals(outString, s);
+    }
+}
